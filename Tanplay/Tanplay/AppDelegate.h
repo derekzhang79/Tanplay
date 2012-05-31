140c95cbefbfe6cc790e28f338a9ff6592ec657b
dingjie <dingjiecn@gmail.com>
2012-05-31 16:13:49 +0800

    add tanplay Xcode project

---
+++ Tanplay/Tanplay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tanplay
+//
+//  Created by ding jie on 5/31/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
